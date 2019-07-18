40d3a1d82da56684a19c437948b5a5337a8d52d9
Anderson Jr <andersonmfjr@gmail.com>
2019-07-18 14:27:21 -0300

    :arrow_up: Add PropTypes dependency

--- src/components/SiderbarList/index.js
+++ src/components/SiderbarList/index.js
@@ -2,50 +2,12 @@ import React from 'react';
 
 import { Container, Item, Divider } from './styles';
 
-const list = [
-  {
-    id: 'all',
-    name: 'All in One',
-  },
-  {
-    id: 'all',
-    name: 'Hacker News',
-  },
-  {
-    id: 'all',
-    name: 'Reddit',
-  },
-  {
-    id: 'all',
-    name: 'Product Hunt',
-  },
-  {
-    id: 'all',
-    name: 'Slashdot',
-  },
-  {
-    id: 'all',
-    name: 'Designer News',
-  },
-  {
-    id: 'all',
-    name: 'Github Trending',
-  },
-  {
-    id: 'all',
-    name: 'Medium',
-  },
-  {
-    id: 'all',
-    name: 'Lifehacker',
-  },
-];
-
-const SiderbarList = () => (
+const SiderbarList = ({ channels }) => (
   <Container>
-    {list.map((item, index) => (
+    <Item>All in One</Item>
+    {channels.map((channel, index) => (
       <>
-        <Item key={item.id}>{item.name}</Item>
+        <Item key={channel.id}>{channel.name}</Item>
         {index === 0 ? <Divider /> : ''}
       </>
     ))}
