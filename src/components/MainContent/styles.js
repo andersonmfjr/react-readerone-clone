2ed83f54f11998eb7df05732a46f4d964b47b802
Anderson Jr <andersonmfjr@gmail.com>
2019-07-18 10:16:43 -0300

    :rocket: Add main title

--- src/components/MainContent/styles.js
+++ src/components/MainContent/styles.js
@@ -7,3 +7,9 @@ export const Container = styled.main`
   height: 100%;
   position: relative;
 `;
+
+export const Title = styled.h1`
+  margin: 30px 30px 10px;
+  font-size: 24px;
+  line-height: 40px;
+`;
