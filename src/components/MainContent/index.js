22c00807d0633ead380b1b7f5c2f531b75e22948
Anderson Jr <andersonmfjr@gmail.com>
2019-07-18 15:15:17 -0300

    :rocket: Add dynamic title in Main Content and Sidebar
    Click on channel toggle title style in sidebar, and change name in Main Content Header

--- src/components/MainContent/index.js
+++ src/components/MainContent/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MainContentLink from '../MainContentLink';
 
 import { Container, Title, LinksContainer, LinksContainerList } from './styles';
 
-const MainContent = () => (
+const MainContent = ({ active }) => (
   <Container>
-    <Title>All In One</Title>
+    <Title>{active.name}</Title>
     <LinksContainer>
       <LinksContainerList>
         <MainContentLink />
@@ -13,5 +14,19 @@ const MainContent = () => (
     </LinksContainer>
   </Container>
 );
+
+MainContent.defaultProps = {
+  active: {
+    name: 'All In One',
+    id: 'all',
+  },
+};
+
+MainContent.propTypes = {
+  active: PropTypes.shape({
+    name: PropTypes.string,
+    id: PropTypes.string,
+  }),
+};
 
 export default MainContent;
