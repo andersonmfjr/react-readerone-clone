bee173a71ffeaa570ea01e6fb038a6b9796a4453
Anderson Jr <andersonmfjr@gmail.com>
2019-07-18 13:51:38 -0300

    :fire: Remove Open Sans font

--- src/styles.js
+++ src/styles.js
@@ -1,19 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
-import woff2 from '../public/fonts/open-sans-v16-latin-regular.woff2';
-import woff from '../public/fonts/open-sans-v16-latin-regular.woff';
 
 // Global Style
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'Open Sans';
-    font-style: normal;
-    font-weight: 400;
-    font-display: fallback;
-    src: local('Open Sans Regular'), local('OpenSans-Regular'),
-        url('${woff2}') format('woff2'),
-        url('${woff}') format('woff');
-  }
-
   * {
     margin: 0;
     padding: 0;
